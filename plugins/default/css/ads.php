29cd4a3131c94759bccac38a8a9438da6c9126f0
lianglee <manactric@gmail.com>
2015-08-14 10:22:48 +0500

    initial release

---
+++ plugins/default/css/ads.php
@@ -0,0 +1,51 @@
+.ossn-ad-tiem-small {
+margin-bottom:20px;
+}
+.ossn-ad-tiem-small img{
+display:block;
+}
+.ossn-ad-tiem-small .descript{
+width: 126px;
+vertical-align: top;
+color: #000;
+font-weight: normal;
+font-size: 12px;
+}
+.ossn-ads .descript {
+width: 126px;
+vertical-align: top;
+color: #000;
+font-weight: normal;
+font-size: 12px;
+}
+.ad-heading {
+width:127px;
+}
+.ad-heading a{
+font-size:12px;
+font-weight:bold;
+}
+.ossn-ad-tiem-small .ossn-ads-link {
+font-size: 11px;
+margin-bottom: 5px;
+margin-top: 3px;
+width: 100px;
+}
+.ossn-ads .sponsered {
+margin-top: 8px;
+margin-bottom: 5px;
+font-weight: bold;
+color: #999;
+}
+.ossn-ads-small {
+width: 125px !important;
+}
+.ossn-ads-link  {
+    font-size: 12px !important;
+    color: #000 !important;
+    font-weight: normal !important;
+}
+.ossn-ad-tiem-small .ad-heading {
+	font-weight:bold;
+    font-size: 13px;
+}
